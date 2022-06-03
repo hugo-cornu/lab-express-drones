00d89b5f6396e48506e96ee21388bccc861b193b
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-03 16:40:56 +0200

    Iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,22 @@
-// Iteration #1
+// Import Mongoose and its methods
+const { Schema, SchemaTypes, model } = require("mongoose");
+
+// Setup the drone schema
+
+const droneSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    propellers: Number,
+    maxSpeed: Number,
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Drone = model("Drone", droneSchema);
+
+module.exports = Drone;
