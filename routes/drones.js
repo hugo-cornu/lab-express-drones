87b854294801fdb83ef756d2fa11c30012b0f1be
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-06-04 15:28:56 +0200

    Done except bonus

--- routes/drones.js
+++ routes/drones.js
@@ -1,26 +1,56 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/drone.model.js");
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", async (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
-})
+  try {
+    const drones = await Drone.find();
+    console.log("DRONES");
+    console.log(drones);
 
-router.post('/drones', (req, res, next) => {
+    res.status(200).json(drones);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/drones", async (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
-})
+  try {
+    const droneToAdd = req.body;
+    const createNewDrone = await Drone.create(droneToAdd);
+    res.status(201).json({ message: "CREATED :)", data: createNewDrone });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/drones/:id', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
-})
+router.patch("/drones/:id", async (req, res, next) => {
+  try {
+    const droneId = req.params.id;
+    const droneUpdated = await Drone.findByIdAndUpdate(droneId, req.body, {
+      new: true,
+    });
+    res.status(200).json(droneUpdated);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
-router.delete('/drones/:id', (req, res, next) => {
+router.delete("/drones/:id", async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
-})
+  try {
+    const droneId = req.params.id;
+    const removedDrone = await Drone.findByIdAndDelete(droneId);
+    console.log(removedDrone);
+    res.status(200).json({ response: "Drone removed", droneRemoved: removedDrone });
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
